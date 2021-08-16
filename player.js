47a4469ac5c80f5229b1090f9f8e20d565b32f98
Prajwal966 <prajwalsoni75@gmail.com>
2021-08-16 05:39:40 +0530

    start

---
+++ player.js
@@ -0,0 +1,180 @@
+  var audioa = document.getElementById('audioa');
+  var playPauseBTNa = document.getElementById('playPauseBTNa');
+  var count = 0;
+
+  function playPausea() {
+    if (count == 0) {
+      count = 1;
+      audioa.play();
+      playPauseBTNa.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audioa.pause();
+      playPauseBTNa.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audiob = document.getElementById('audiob');
+  var playPauseBTNb = document.getElementById('playPauseBTNb');
+  var count = 0;
+  function playPauseb() {
+    if (count == 0) {
+      count = 1;
+      audiob.play();
+      playPauseBTNb.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audiob.pause();
+      playPauseBTNb.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audioc = document.getElementById('audioc');
+  var playPauseBTNc = document.getElementById('playPauseBTNc');
+  var count = 0;
+  function playPausec() {
+    if (count == 0) {
+      count = 1;
+      audioc.play();
+      playPauseBTNc.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audioc.pause();
+      playPauseBTNc.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audiod = document.getElementById('audiod');
+  var playPauseBTNd = document.getElementById('playPauseBTNd');
+  var count = 0;
+  function playPaused() {
+    if (count == 0) {
+      count = 1;
+      audiod.play();
+      playPauseBTNd.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audiod.pause();
+      playPauseBTNd.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audioe = document.getElementById('audioe');
+  var playPauseBTNe = document.getElementById('playPauseBTNe');
+  var count = 0;
+  function playPausee() {
+    if (count == 0) {
+      count = 1;
+      audioe.play();
+      playPauseBTNe.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audioe.pause();
+      playPauseBTNe.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audioz = document.getElementById('audioz');
+  var playPauseBTNz = document.getElementById('playPauseBTNz');
+  var count = 0;
+  function playPausez() {
+    if (count == 0) {
+      count = 1;
+      audioz.play();
+      playPauseBTNz.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audioz.pause();
+      playPauseBTNz.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audiog = document.getElementById('audiog');
+  var playPauseBTNg = document.getElementById('playPauseBTNg');
+  var count = 0;
+  function playPauseg() {
+    if (count == 0) {
+      count = 1;
+      audiog.play();
+      playPauseBTNg.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audiog.pause();
+      playPauseBTNg.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audioh = document.getElementById('audioh');
+  var playPauseBTNh = document.getElementById('playPauseBTNh');
+  var count = 0;
+  function playPauseh() {
+    if (count == 0) {
+      count = 1;
+      audioh.play();
+      playPauseBTNh.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audioh.pause();
+      playPauseBTNh.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audioi = document.getElementById('audioi');
+  var playPauseBTNi = document.getElementById('playPauseBTNi');
+  var count = 0;
+  function playPausei() {
+    if (count == 0) {
+      count = 1;
+      audioi.play();
+      playPauseBTNi.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audioi.pause();
+      playPauseBTNi.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audioj = document.getElementById('audioj');
+  var playPauseBTNj = document.getElementById('playPauseBTNj');
+  var count = 0;
+  function playPausej() {
+    if (count == 0) {
+      count = 1;
+      audioj.play();
+      playPauseBTNj.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audioj.pause();
+      playPauseBTNj.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audiok = document.getElementById('audiok');
+  var playPauseBTNk = document.getElementById('playPauseBTNk');
+  var count = 0;
+  function playPausek() {
+    if (count == 0) {
+      count = 1;
+      audiok.play();
+      playPauseBTNk.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audiok.pause();
+      playPauseBTNk.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
+
+  var audiol = document.getElementById('audiol');
+  var playPauseBTNl = document.getElementById('playPauseBTNl');
+  var count = 0;
+  function playPausel() {
+    if (count == 0) {
+      count = 1;
+      audiol.play();
+      playPauseBTNl.innerHTML = '<ion-icon name="pause-outline"></ion-icon>';
+    } else {
+      count = 0;
+      audiol.pause();
+      playPauseBTNl.innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    }
+  }
---
+++ sign.js
@@ -0,0 +1,9 @@
+function check(){
+    var p = document.getElementById("pass").value;
+    if(p == "purvi_goyall"){
+        window.open("home.html");
+    }
+    else{
+        alert("Password Incorrect");
+    }
+}
